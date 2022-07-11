c1b04b986ca326e19e88af20e4f4c2a0810b207f
lcydr <1211753823@qq.com>
2022-07-11 17:26:38 +0800

    新

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import { Tabbar, TabbarItem, NavBar } from 'vant';
+Vue.use(Tabbar)
+Vue.use(TabbarItem)
+Vue.use(NavBar)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="layout">
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="search">搜索</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
